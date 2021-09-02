23aa5394e7ab1d7b345aceb575c5fd80bde65d20
Erick Villalobos <erickvilla1142@gmail.com>
2021-09-01 18:49:08 -0600

    Inicializado Proyecto

---
+++ src/proyectogrupo1/ProyectoGrupo1.java
@@ -0,0 +1,12 @@
+package proyectogrupo1;
+
+import Formularios.FrmBienvenida;
+
+public class ProyectoGrupo1 {
+
+    public static void main(String[] args) {
+         FrmBienvenida form = new FrmBienvenida();
+        form.setVisible(true);
+    }
+    
+}
